171174246d671a1589eaf699628cf08d2b16bceb
angeljr196 <angeljr196@gmail.com>
2019-11-18 13:53:25 +0100

    script con hola mundo

---
+++ prim_script.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+#DESCRIPTION yo te digo lo que hago
+#AUTHOR uo te digo quien lo escribió
+echo "hola mundo"
+exit 0 #yo soy un comentario al final de linea
+#exit 0 indica que todo termino bien
